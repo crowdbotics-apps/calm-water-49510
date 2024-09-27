2076a4a34d62ff9c7b6967e54462b06eea2f1333
Crowdbotics <team@crowdbotics.com>
2024-09-27 10:23:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-water-49510",
+  url: "https://calm-water-49510.botics.co",
+  api: "https://calm-water-49510.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
